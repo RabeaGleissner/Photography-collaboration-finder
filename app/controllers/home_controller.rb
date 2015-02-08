a13281ae8ee5552cd5274c1ae75111e8fc03fc92
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-08 20:40:21 +0000

    displaying Flickr set images in view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,24 @@
 class HomeController < ApplicationController
-  
+
+  before_action :flickr_keys
+
   def index
-    
+   @photos = flickr.photosets.getPhotos(photoset_id: '72157626714345858').photo
   end
-  
+
   def about
-    
+
   end
+
+
+
+  private
+
+  def flickr_keys
+    FlickRaw.api_key       = ENV['FLICKR_API_KEY']
+    FlickRaw.shared_secret = ENV['FLICKR_SECRET_API_KEY'] 
+  end
+
+ 
 
 end
---
+++ app/models/flickr.rb
@@ -0,0 +1,5 @@
+class Flickr < FlickRaw::Flickr
+
+
+
+end
