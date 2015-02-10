8611bead765d69992ad5c26300edefbd2bfcd305
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-10 17:06:14 +0000

    added seeds for film formats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Film.delete_all
+
+i1 = Film.create!(format: '35mm')
+i2 = Film.create!(format: '120')
+i3 = Film.create!(format: '220')
