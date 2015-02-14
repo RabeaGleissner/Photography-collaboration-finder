29faae1f3ca7011c609b85911136502c5ffaf6a1
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-14 20:15:06 +0000

    changed user db table to include fields for latitude and longitude

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213162819) do
+ActiveRecord::Schema.define(version: 20150214201412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 20150213162819) do
     t.string   "image"
     t.string   "profileurl"
     t.string   "role"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
