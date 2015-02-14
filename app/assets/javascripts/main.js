6e42785950656dd1489487847ca558a1df832135
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-14 20:55:22 +0000

    implemented google maps api

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,18 @@
 var myMap = myMap || {};
 
-myMap.initialize = function(){
-  var mapOptions = {
-    center: { lat: 51.52, lng: -0.115 },
-    zoom: 5,
+myMap.initialize = function() {
 
-  };
+    var mapOptions = {
+        center: { lat:  51.52, lng: -0.115},     
+        zoom: 14,
+        mapTypeId:google.maps.MapTypeId.ROADMAP //default
+    };
 
-  myMap.map = new google.maps.Map(myMap.mapElement, mapOptions);
+    var map = new google.maps.Map(myMap.mapCanvas, mapOptions);
 
 }
+
+$(function(){
+  myMap.mapCanvas = $('#map-canvas')[0];
+  myMap.initialize();
+});
