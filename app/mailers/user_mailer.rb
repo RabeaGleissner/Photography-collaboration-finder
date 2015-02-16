ff0417cf281ebc919c2e5510af4828fafba58b46
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-16 20:59:22 +0000

    created html file for email template and updated user_mailer model

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ActionMailer::Base
  default from: "from@example.com"
 
      def partner_alert(user)
+      @user = user
        mail(to: user.email, subject: "Film Swapsters: You have a new film partner")
      end
 end
