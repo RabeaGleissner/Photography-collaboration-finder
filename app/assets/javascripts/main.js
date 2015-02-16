a4c426f8784d59de9633a80840de3c30af5abbef
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-16 14:08:24 +0000

    displays user's location on map

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,23 @@
 var myMap = myMap || {};
 
+var latitude;
+var longitude;
+
+$(document).ready(function() {
+
+  latitude = parseFloat($('#latitude').text());
+
+  longitude = parseFloat($('#longitude').text());
+
+  console.log(latitude);
+  console.log(longitude);
+ });
+
+
 myMap.initialize = function() {
 
     var mapOptions = {
-        center: { lat:  51.52, lng: -0.115},     
+        center: { lat: latitude, lng: longitude},     
         zoom: 14,
         mapTypeId:google.maps.MapTypeId.ROADMAP //default
     };
@@ -13,6 +27,8 @@ myMap.initialize = function() {
 }
 
 $(function(){
+  $
   myMap.mapCanvas = $('#map-canvas')[0];
   myMap.initialize();
 });
+
