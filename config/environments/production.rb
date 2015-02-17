9f519c31167841ca48eafee4c0e39ebb0ac1f06a
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-17 18:24:35 +0000

    updated mandrill settings for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,19 +14,22 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { host: 'film-swap.herokuapp.com' }
+  
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'film-swap.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
     port: 587,
-    user_name: "MANDRILL_USERNAME",
-    password: "MANDRILL_API_KEY"
+    user_name: ENV['MANDRILL_USERNAME'],
+    password: ENV['MANDRILL_API_KEY'],
+    domain: 'heroku.com', 
+    authentication: :plain
   }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
