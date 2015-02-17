137f4e029feb087a6671cd449bf7ac3fe7c7ee2a
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-17 12:29:49 +0000

    added database query for user's collaborations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
 
   def list
 
-    @collaborations = Collaboration.where(collaborator1_id: current_user.id, collaborator2_id: current_user.id)
-  end
+    @collaborations = Collaboration.where("collaborator1_id = ? or collaborator2_id = ?", current_user.id, current_user.id).uniq
+end
 
   def show
     @user = User.find(params[:id])
