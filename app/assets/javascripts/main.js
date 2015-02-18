a3d80319f95b213f8a6d2c2e6ecb3a594a10b870
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-18 19:02:35 +0000

    updated styles

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -16,18 +16,18 @@ $(function(){
   $('#more_photos').
   on('ajax:success',function(evt, data, status, xhr){
    $('.flex_container').append(data);
-  var new_page = parseInt($('#page_param').val())+1;
-  $('#page_param').val(new_page);
-  
+   var new_page = parseInt($('#page_param').val())+1;
+   $('#page_param').val(new_page);
+
  }).
   on('ajax:error',function(xhr, status, error){
    console.log('failed:', error);
  });
 
-  $('#click').click(function(ev){
+  $('nav ul li#menu').on("click", function(ev){
     console.log('clicked');
-    ev.preventDefault();
-    $('#account').toggle();
+    // ev.preventDefault();
+    $(this).children("ul").toggle();
   });
 
 
